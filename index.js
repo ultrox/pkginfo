15d23c5f615702b694bdee8cecfb7b21306775c4
Marko <crashxx@gmail.com>
2019-02-19 23:59:53 +0100

    feat(*): fix lint errors add proper verification to package name
    WARNING: this is not atomic change, pretty early in while prototyping.
    BRAKING CHANGE

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
 // partially inspired from AlessandroMinoccheri/package-info
-var got = require('got')
-var { getDepLength } = require('./utils')
+const got = require('got')
+const { getDepLength, isValidPkgName } = require('./utils')
 
-module.exports = function(pckg, opts = ['name', 'version', 'description']) {
+module.exports = function pkginfo(
+  pckg,
+  opts = ['name', 'version', 'description'],
+) {
   // I'm nice like that
   if (typeof pckg === 'string') {
     pckg = { name: pckg }
   }
-  if (pckg.name && typeof pckg.name !== 'string') {
-    return Promise.reject(new Error('package name required'))
-  }
-  return got('https://registry.npmjs.org/' + pckg.name.toLowerCase())
-    .then(function(data) {
-      const dataParsed = JSON.parse(data.body)
-      const name = dataParsed.name
-      const version = pckg.version || dataParsed['dist-tags'].latest
-      const latest = dataParsed['dist-tags'].latest
-      const description = dataParsed.description
-      const license = dataParsed.license
-      let homepage = ''
-      let author = ''
 
-      // edge: when there is no version
-      if (!dataParsed.versions[version]) {
-        return { name: `${version} version of ${pckg.name} doesn't exist` }
-      }
+  if (isValidPkgName(pckg.name)) {
+    return got(`https://registry.npmjs.org/${pckg.name.toLowerCase()}`)
+      .then(data => {
+        const dataParsed = JSON.parse(data.body)
+        const name = dataParsed.name
+        const version = pckg.version || dataParsed['dist-tags'].latest
+        const latest = dataParsed['dist-tags'].latest
+        const description = dataParsed.description
+        const license = dataParsed.license
+        let homepage = ''
+        let author = ''
 
-      const dependencies = getDepLength(dataParsed, version)
-      const devDependencies = getDepLength(
-        dataParsed,
-        version,
-        'devDependencies',
-      )
+        // edge: when there is no version
+        if (!dataParsed.versions[version]) {
+          return { name: `${version} version of ${pckg.name} doesn't exist` }
+        }
 
-      if (dataParsed.homepage !== undefined) {
-        homepage = dataParsed.homepage
-      }
+        const dependencies = getDepLength(dataParsed, version)
+        const devDependencies = getDepLength(
+          dataParsed,
+          version,
+          'devDependencies',
+        )
 
-      // get author and maintainers
-      if (dataParsed.author && dataParsed.author.name) {
-        // call it a day I have author
-        author = dataParsed.author.name
-      } else {
-        if (Array.isArray(dataParsed.maintainers)) {
+        if (dataParsed.homepage !== undefined) {
+          homepage = dataParsed.homepage
+        }
+
+        // get author and maintainers
+        if (dataParsed.author && dataParsed.author.name) {
+          // call it a day I have author
+          author = dataParsed.author.name
+        } else if (Array.isArray(dataParsed.maintainers)) {
           // TODO: maybe make them unique
           author = dataParsed.maintainers
-            .reduce((acc, { name }) => (name ? `${name} ${acc}` : acc), '')
+            .reduce((acc, { name: n }) => (n ? `${n} ${acc}` : acc), '')
             .trim()
+        } else {
+          // is not neede, author is empty str
         }
-      }
-      const allProps = {
-        homepage,
-        latest,
-        name,
-        description,
-        version,
-        author,
-        license,
-        devDependencies,
-        dependencies,
-      }
 
-      const output = {}
-      for (let option of opts) {
-        let value = allProps[option]
-        value && (output[option] = value)
-      }
-      return output
-    })
-    .catch(function(err) {
-      // I don't want to throw because I need this info as well
-      if (err.statusCode === 404) {
-        return { name: `Package ${pckg.name} doesn't exist` }
-      }
-      throw err
-    })
+        const allProps = {
+          homepage,
+          latest,
+          name,
+          description,
+          version,
+          author,
+          license,
+          devDependencies,
+          dependencies,
+        }
+
+        const output = {}
+        for (let option of opts) {
+          let value = allProps[option]
+          value && (output[option] = value)
+        }
+        return output
+      })
+      .catch(err => {
+        // I don't want to throw because I need this info as well
+        if (err.statusCode === 404) {
+          return { name: `Package ${pckg.name} doesn't exist` }
+        }
+        throw err
+      })
+  } else {
+    return Promise.reject(new Error('package name required'))
+  }
 }
--- utils.js
+++ utils.js
@@ -1,4 +1,10 @@
 module.exports = {
+  isValidPkgName: str =>
+    typeof str === 'string' && str.length > 0 && str.length <= 214,
+  // TODO: https://docs.npmjs.com/files/package.json#name
+  // The name can’t start with a dot or an underscore
+  // New packages must not have uppercase letters in the name.
+  // can’t contain any non-URL-safe characters.
   getDepLength: (data, version, devDependencies) => {
     const depKey = devDependencies || 'dependencies'
     return Object.keys(data.versions[version][depKey] || []).length
