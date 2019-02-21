b8089ac85d735f4e9d5b60a54d5ac66217e9ee01
Marko <crashxx@gmail.com>
2019-02-21 03:08:08 +0100

    refactor(testing): mock to use jest.fn

--- __mocks__/got.js
+++ __mocks__/got.js
@@ -1,20 +1,21 @@
-module.exports = () => {
-  return Promise.resolve({
-    body: `{
-      "id": "axo", 
+const defaultResponse = {
+  body: `{
       "name": "axios",
       "description": "Hello",
       "dist-tags": {"latest": "10.0.0"},
       "author": {"name": "Joe Doe"},
+      "maintainers": [{
+        "name": "mzabriskie",
+        "email": "mzabriskie@gmail.com"}],
       "versions": {
       "10.0.0": {
          "name": "axios",
-         "author": {"name": "Joe Doe"},
-         "maintainers": [{
-         "name": "mzabriskie",
-         "email": "mzabriskie@gmail.com"
-        }]}
+         "author": {"name": "Joe Doe"}
+        }
       }
     }`,
-  })
 }
+
+const got = jest.fn(() => Promise.resolve(defaultResponse))
+
+module.exports = got
