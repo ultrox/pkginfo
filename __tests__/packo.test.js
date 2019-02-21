67c24ef3021a0356e99a9d90a77da00db00251ff
Marko <crashxx@gmail.com>
2019-02-21 03:13:50 +0100

    chore(testing): add all tests

--- __tests__/packo.test.js
+++ __tests__/packo.test.js
@@ -1,27 +1,63 @@
+const got = require('got')
 const pkginfo = require('../index')
 
+const pkgname = 'axios'
+
 test('return sane msg if package name empty str', () => {
+  // eslint-disable-next-line
   pkginfo('').catch(e => {
     expect(e.message).toBe('package name required')
   })
 })
 
 test('accept string and obj as first arg', async () => {
-  const pkgname = 'axios'
   const axiosinfo = await pkginfo(pkgname)
-  const axiosObjInfo = await pkginfo({ name: pkgname })
-
   expect(axiosinfo.name).toBe(pkgname)
-  expect(axiosObjInfo.name).toBe(pkgname)
 })
 
 test('given maintainers & author name prefer author name', async () => {
-  const pkgname = 'runner'
   const axiosinfo = await pkginfo(pkgname, ['author', 'name', 'description'])
   expect(axiosinfo.author).toBe('Joe Doe')
 })
-// need to inject scenario
-test.todo('returns maintainers as string when no author name')
-test.todo('if version not provided uses latest as version')
-test.todo('return null if 404')
-test.todo("return null if can't find version")
+
+test('returns maintainers as string when no author name', async () => {
+  const mockdata = {
+    body: `{
+      "dist-tags": {"latest": "10.0.0"},
+      "maintainers": [ {"name": "Joe Doe"}, {"name": "jane"} ],
+      "versions": { "10.0.0": { "name": "axios" } }
+    }`,
+  }
+
+  got.mockImplementationOnce(() => Promise.resolve(mockdata))
+  const axiosinfo = await pkginfo({ name: pkgname }, ['author'])
+  expect(axiosinfo.author).toBe('jane Joe Doe')
+})
+
+test('if version provided provided uses specify version', async () => {
+  const mockdata = {
+    body: `{
+      "dist-tags": {"latest": "10.0.0"},
+      "versions": {"1.0.0": {"name": "axios"}}
+    }`,
+  }
+
+  got.mockImplementationOnce(() => Promise.resolve(mockdata))
+  const axiosinfo = await pkginfo({ name: pkgname, version: '1.0.0' }, [
+    'version',
+  ])
+  expect(axiosinfo.version).toBe('1.0.0')
+})
+
+test('return null if no version', async () => {
+  const mockdata = {
+    body: `{
+      "dist-tags": {"latest": "10.0.0"},
+      "versions": {}
+    }`,
+  }
+
+  got.mockImplementationOnce(() => Promise.resolve(mockdata))
+  const axiosinfo = await pkginfo(pkgname)
+  expect(axiosinfo).toBe(null)
+})
